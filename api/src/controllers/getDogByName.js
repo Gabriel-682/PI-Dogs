c19367b52c2f181926dec3b152a4e6348fe23c30
Gabriel-682 <gabriel.682681@gmail.com>
2023-05-11 19:49:47 -0300

    GetDogByName

--- api/src/controllers/getDogByName.js
+++ api/src/controllers/getDogByName.js
@@ -1,8 +1,31 @@
 const axios = require("axios");
+const { Dog } = require("../db");
+const { Temperament } = require("../db");
 
-const getDogByName = async (name) => {
-    const dog = await axios(`http://localhost:3001/dogs/name?name=${name}`)
-  return "Hola";
+const getDogByName = async (name, isApi) => {
+  if (isApi === "api") {
+    const { data } = await axios(
+      `https://api.thedogapi.com/v1/breeds/search?q=${name}`
+    );
+    return data;
+  }
+  if (isApi === "db") {
+    const dog = await Dog.findOne({
+      where: { name },
+      include: {
+        model: Temperament,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    });
+
+    if (!dog) throw Error(`La raza ${name} no existe, puede crearla.`);
+
+    return dog;
+  }
+  throw Error("Debe indicar si la consulta es para DB o API.");
 };
 
 module.exports = getDogByName;
--- api/src/handlers/getDogByNameHandler.js
+++ api/src/handlers/getDogByNameHandler.js
@@ -2,8 +2,8 @@ const { getDogByName } = require("../controllers/index");
 
 const getDogByNameHandler = async (req, res) => {
   try {
-    const { name } = req.query;
-    const dog = await getDogByName(name);
+    const { name, isApi } = req.query;
+    const dog = await getDogByName(name, isApi);
     res.status(200).json(dog);
   } catch (error) {
     res.status(400).json({ error: error.message });
