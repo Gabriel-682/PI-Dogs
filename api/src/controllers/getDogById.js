40c795b45313d6452ffe2b3a6256d7c490492020
Gabriel-682 <gabriel.682681@gmail.com>
2023-05-11 19:16:20 -0300

    GetDogById

--- api/src/controllers/getDogById.js
+++ api/src/controllers/getDogById.js
@@ -1,25 +1,22 @@
 const axios = require("axios");
 const { Dog } = require("../db");
-
-// let isApi = true; //Eliminar.-
+const { Temperament } = require("../db");
 
 const getDogById = async (id, isApi) => {
   if (isApi === "api") {
     const { data } = await axios(`https://api.thedogapi.com/v1/breeds/${id}`);
-    // isApi = !isApi; // Eliminar.-
     return data;
   }
   if (isApi === "db") {
     const dog = await Dog.findByPk(id, {
-      // include: {
-      //   model: Temperament,
-      //   attributes: ["name"],
-      //   through: {
-      //     attributes: [],
-      //   },
-      // },
+      include: {
+        model: Temperament,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
     });
-    // isApi = !isApi; // Eliminar.-
     if (!dog) throw Error("El ID no existe, ingrese otro.");
     return dog;
   }
