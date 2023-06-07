bcbbee0a236269327e7363535ca011aea567904b
Gabriel-682 <gabriel.682681@gmail.com>
2023-06-06 23:28:30 -0300

    Presented

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -3,6 +3,7 @@ const regexUrl =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/;
 
 export default function validation(newDogInput, allDogsNames) {
+  
   let errors = {};
 
   if (newDogInput.name !== "") {
